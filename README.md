9c825dfbaa246fdfd9ec8f11e1da9cd6f006b9de
Robin <Robin@PF1K73HP.PERS.PXL.LOCAL>
2023-10-20 15:27:19 +0200

    solution

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Basisfunctionaliteiten
 
-Zorg ervoor dat je de eigenschappen die je in de vorige opdracht hebt aangemaakt linkt aan de HTML code (titel aan een h1 element en intro aan een p element) en bekijk het resultaat in de browser.
+Voorzie in de assets map een afbeelding naar keuze. Voeg vervolgens binnen een img tag toe en zorg ervoor dat je het src attribuut bindt met een variabele die als waarde het pad naar de afbeelding heeft.
+Spreek dit element aan binnen de style tag van het component en geef het een breedte van 75%.
+
 
 
--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,38 @@ export default {
   data() {
     return { 
       titel: "Vue basisfunctionaliteiten",
-      intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      // De variabele voor de afbeelding. Opgelet de @ alias werkt hier niet omdat vue de waarde als een volwaardige string gaat benaderen
+      baseImage: './src/assets/car.jpg'
     }
   }
 }
 </script>
 
 <template>
-  <!-- variabelen tonen dmv data binding -->
-  <h1>{{ titel }}</h1>
-  <p>{{ intro }}</p>
+  <div class="container">
+    <h1>{{ titel }}</h1>
+    <p>{{ intro }}</p>
+    <picture>
+      <!-- Met v-bind kan je de variabele binden aan het src attribuut -->
+      <img class="main-image" v-bind:src="baseImage">
+    </picture>
+  </div>
 </template>
 
 <style>
+/* Basis css toegevoegd om het er allemaal wat beter te laten uitzien */
+.container {
+  width: 75%;
+  margin: auto;
+  padding-bottom: 5rem;
+}
+picture {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.main-image {
+  width: 50%;
+}
 </style>
